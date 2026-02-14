907b4c605185d7183e275aca41b274ede7767a77
rahulkumar40 <rahul4004kumar@gmail.com>
2026-02-14 23:54:13 +0530

    Interview Guide Map filter reduce 2

--- map_filter_reduce/filter.js
+++ map_filter_reduce/filter.js
@@ -13,3 +13,18 @@ const otp = arr.filter((x)=>{
     return x<4;
 })
 console.log(otp)
+
+
+const users =[
+    {firstName:"Rahul",lastName:"kumar",age:23},
+    {firstName:"Rohan",lastName:"kumar",age:25},
+    {firstName:"Ramu",lastName:"kumar",age:23},
+    {firstName:"Ronit",lastName:"J",age:44},
+]
+
+// find first name those age is less then 30
+const outputFirstName = users.filter(function(user){
+    if(user.age<30)return user;
+    
+}).map(x=>x.firstName)
+console.log(outputFirstName)
